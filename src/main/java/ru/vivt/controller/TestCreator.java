466c125e9a2d9b0fae11902f3a7ce693b0b92535
RedCrabVb <cany245@mail.ru>
2021-11-29 13:40:52 +0300

    Get parameter

--- src/main/java/ru/vivt/controller/TestCreator.java
+++ src/main/java/ru/vivt/controller/TestCreator.java
@@ -2,8 +2,12 @@ package ru.vivt.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class TestCreator {
@@ -11,5 +15,12 @@ public class TestCreator {
     public String testCreatorPage(@RequestParam String token, Model model) {
         model.addAttribute("message", token);
         return "test";
+    }
+
+    @GetMapping("/testAdd")
+    public String testAdd(@RequestParam Map<String,String> allRequestParams, ModelMap model) {
+        System.out.println(allRequestParams.toString());
+        System.out.println(allRequestParams.keySet().size());
+        return "result";
     }
 }
